dd252c47adbb4ada8fd2eec691ef8673108a9195
Supraav <supravadhikari@example.com>
2026-02-04 22:36:15 +0545

    requirements

---
+++ src/ingestion_pipeline.py
@@ -0,0 +1,12 @@
+import os 
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings 
+from pathlib import Path
+
+load_dotenv()
+
+def load_pdfs(directory_path):
+    pass
